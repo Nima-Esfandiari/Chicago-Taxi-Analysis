e993afcdc87b00525f4c9f540e25b07a1a2ec497
Nima-Esfandiari <124087943+Nima-Esfandiari@users.noreply.github.com>
2026-07-01 21:44:35 -0700

    Delete sql/06_kpi_dashboard.sql

--- sql/06_kpi_dashboard.sql
+++
@@ -1,8 +0,0 @@
--- Dashboard KPIs
-
-SELECT
-    COUNT(*) AS total_trips,
-    ROUND(SUM(trip_total),2) AS total_revenue,
-    ROUND(AVG(trip_miles),2) AS avg_trip_distance,
-    ROUND(AVG(trip_total),2) AS avg_revenue_per_trip
-FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`;
